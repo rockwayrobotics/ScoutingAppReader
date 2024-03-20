1bf932eed26fecb44ff0f2555930bee223e6515d
Tnixc <tnixxc@gmail.com>
2024-03-20 12:27:29 -0400

    format

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,56 @@
 const cacheName = "cache1"; // Change value to force update
 
-self.addEventListener("install", event => {
-	// Kick out the old service worker
-	self.skipWaiting();
+self.addEventListener("install", (event) => {
+  // Kick out the old service worker
+  self.skipWaiting();
 
-	event.waitUntil(
-		caches.open(cacheName).then(cache => {
-			return cache.addAll([
-				"/",
-                "index.html",
-                "manifest.json",
-				"html5-qrcode.min.js",
-                "style.css",
-                "icons/manifest-icon-192.maskable.png",
-                "icons/manifest-icon-512.maskable.png",
-                "icons/apple-icon-180.png",
-			]);
-		})
-	);
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll([
+        "/",
+        "index.html",
+        "manifest.json",
+        "html5-qrcode.min.js",
+        "style.css",
+        "icons/manifest-icon-192.maskable.png",
+        "icons/manifest-icon-512.maskable.png",
+        "icons/apple-icon-180.png",
+      ]);
+    }),
+  );
 });
 
-self.addEventListener("activate", event => {
-	// Delete any non-current cache
-	event.waitUntil(
-		caches.keys().then(keys => {
-			Promise.all(
-				keys.map(key => {
-					if (![cacheName].includes(key)) {
-						return caches.delete(key);
-					}
-				})
-			)
-		})
-	);
+self.addEventListener("activate", (event) => {
+  // Delete any non-current cache
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      Promise.all(
+        keys.map((key) => {
+          if (![cacheName].includes(key)) {
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
 });
 
 // Offline-first, cache-first strategy
 // Kick off two asynchronous requests, one to the cache and one to the network
 // If there's a cached version available, use it, but fetch an update for next time.
-// Gets data on screen as quickly as possible, then updates once the network has returned the latest data. 
-self.addEventListener("fetch", event => {
-	event.respondWith(
-		caches.open(cacheName).then(cache => {
-			return cache.match(event.request).then(response => {
-				return response || fetch(event.request).then(networkResponse => {
-					cache.put(event.request, networkResponse.clone());
-					return networkResponse;
-				});
-			})
-		})
-	);
+// Gets data on screen as quickly as possible, then updates once the network has returned the latest data.
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return (
+          response ||
+          fetch(event.request).then((networkResponse) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+        );
+      });
+    }),
+  );
 });
